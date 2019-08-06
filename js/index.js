33ae65b1c932c16f9f8c3531b4dc721339106f8d
TÉCNICO EM INFORMÁTICA 2019.1 <36121742019.1@senacrjedu.local>
2019-08-06 10:39:17 -0300

    Atualizar java script do css

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
 /* Javascript do site */
 
 console.log ("Aplicação Iniciada")
+
+var img = documento.getElementsByTagname('img')[0];
+img = document.addEventListener(img, 'click', function){
+    alert('clicou');
+}
